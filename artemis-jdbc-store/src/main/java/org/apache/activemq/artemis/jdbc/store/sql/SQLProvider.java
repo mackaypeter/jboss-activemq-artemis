franz1981/activemq-artemis

ARTEMIS-1447 JDBC NodeManager to support JDBC HA Shared Store

artemis-jdbc-store/src/main/java/org/apache/activemq/artemis/jdbc/store/sql/SQLProvider.java
@@ -19,7 +19,7 @@ package org.apache.activemq.artemis.jdbc.store.sql;
 public interface SQLProvider {
 
    enum DatabaseStoreType {
-      PAGE, MESSAGE_JOURNAL, BINDINGS_JOURNAL, LARGE_MESSAGE
+      PAGE, MESSAGE_JOURNAL, BINDINGS_JOURNAL, LARGE_MESSAGE, NODE_MANAGER
    }
 
    long getMaxBlobSize();
@@ -62,7 +62,44 @@ public interface SQLProvider {
 
    boolean closeConnectionOnShutdown();
 
+   String createNodeManagerStoreTable();
+
+   String insertStateOnNodeManagerStoreTable();
+
+   String insertNodeIdOnNodeManagerStoreTable();
+
+   String insertLiveLockOnNodeManagerStoreTable();
+
+   String insertBackupLockOnNodeManagerStoreTable();
+
+   String tryLiveLockOnNodeManagerStoreTable();
+
+   String tryBackupLockOnNodeManagerStoreTable();
+
+   String tryReleaseLiveLockOnNodeManagerStoreTable();
+
+   String tryReleaseBackupLockOnNodeManagerStoreTable();
+
+   String isLiveLockedOnNodeManagerStoreTable();
+
+   String isBackupLockedOnNodeManagerStoreTable();
+
+   String renewLiveLockOnNodeManagerStoreTable();
+
+   String renewBackupLockOnNodeManagerStoreTable();
+
+   String currentTimestampOnNodeManagerStoreTable();
+
+   String updateStateOnNodeManagerStoreTable();
+
+   String selectStateOnNodeManagerStoreTable();
+
+   String updateNodeIdOnNodeManagerStoreTable();
+
+   String selectNodeIdOnNodeManagerStoreTable();
+
    interface Factory {
+
       SQLProvider create(String tableName, DatabaseStoreType dbStoreType);
    }
 }

mackaypeter/jboss-activemq-artemis

(cherry picked from commit 7944a25269d939791bfbc2637e3c649a9137ad45)
(cherry picked from commit bae4dae9dafab372e18c15e2a0cf45ccd5daaa3f)

artemis-jdbc-store/src/main/java/org/apache/activemq/artemis/jdbc/store/sql/SQLProvider.java
@@ -19,7 +19,7 @@ package org.apache.activemq.artemis.jdbc.store.sql;
 public interface SQLProvider {
 
    enum DatabaseStoreType {
-      PAGE, MESSAGE_JOURNAL, BINDINGS_JOURNAL, LARGE_MESSAGE
+      PAGE, MESSAGE_JOURNAL, BINDINGS_JOURNAL, LARGE_MESSAGE, NODE_MANAGER
    }
 
    long getMaxBlobSize();
@@ -62,7 +62,44 @@ public interface SQLProvider {
 
    boolean closeConnectionOnShutdown();
 
+   String createNodeManagerStoreTableSQL();
+
+   String createStateSQL();
+
+   String createNodeIdSQL();
+
+   String createLiveLockSQL();
+
+   String createBackupLockSQL();
+
+   String tryAcquireLiveLockSQL();
+
+   String tryAcquireBackupLockSQL();
+
+   String tryReleaseLiveLockSQL();
+
+   String tryReleaseBackupLockSQL();
+
+   String isLiveLockedSQL();
+
+   String isBackupLockedSQL();
+
+   String renewLiveLockSQL();
+
+   String renewBackupLockSQL();
+
+   String currentTimestampSQL();
+
+   String writeStateSQL();
+
+   String readStateSQL();
+
+   String writeNodeIdSQL();
+
+   String readNodeIdSQL();
+
    interface Factory {
+
       SQLProvider create(String tableName, DatabaseStoreType dbStoreType);
    }
 }